b670e82e0d920511e74329e20e93b99b7df26296
Kevin Mildau <kevin.mildau@gmail.com>
2022-11-15 15:09:48 +0100

    moved expand and threshold setters

--- utils/parsing.py
+++ utils/parsing.py
@@ -20,3 +20,29 @@ def update_class(selected_class, class_dict):
     colors = visual_utils.construct_grey_palette(n_colors, white_buffer)
     color_dict = visual_utils.create_color_dict(colors, selected_class_data)
     return selected_class_data, color_dict
+
+def update_expand_level(new_expand_level):
+    if (new_expand_level 
+        and new_expand_level >= 1 
+        and new_expand_level <= 6 
+        and isinstance(new_expand_level, int)):
+        new_placeholder = ('Expand Level 1 =< thr <= 6,'
+            f'current: {new_expand_level}')
+        return new_expand_level, new_placeholder
+    else:
+        default_expand_level = 1
+        default_placeholder = "Expand Level 1 =< thr <= 6, def. 1"
+        return default_expand_level,  default_placeholder
+
+def update_threshold(new_threshold):
+    if (new_threshold 
+        and new_threshold < 1 
+        and new_threshold > 0
+        and isinstance(new_threshold, float)):
+        new_placeholder = ('Threshold 0 < thr < 1,' 
+            f'current: {new_threshold}')
+        return new_threshold, new_placeholder
+    else:
+        default_threshold = 0.9
+        default_placeholder = "Threshold 0 < thr < 1, def. 0.9"
+        return default_threshold,  default_placeholder
