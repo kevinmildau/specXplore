662e4315f12d418bb9b5335d6e7cbcb590634fc5
Kevin Mildau <kevin.mildau@gmail.com>
2022-11-15 10:33:53 +0100

    styling

--- utils/parsing.py
+++ utils/parsing.py
@@ -1,8 +1,11 @@
-
 def extract_identifiers(plotly_selection_data):
-    """ Function extracts custom_data id's from a provided point selection dictionary. """
+    """ Function extracts custom_data id's from a provided point selection 
+        dictionary. """
     if plotly_selection_data != None:
-        selected_ids = [elem["customdata"][0] for elem in plotly_selection_data["points"]]
+        selected_ids = [
+            elem["customdata"][0] 
+            for elem 
+            in plotly_selection_data["points"]]
         print("slected _ids:", selected_ids)
     else:
         selected_ids = []
