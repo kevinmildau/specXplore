f74e57bfbdd8fe70e4b104f9b54aef57569f09f5
Kevin Mildau <kevin.mildau@gmail.com>
2022-11-15 12:26:24 +0100

    moved update class code to module

--- utils/parsing.py
+++ utils/parsing.py
@@ -10,3 +10,11 @@ def extract_identifiers(plotly_selection_data):
     else:
         selected_ids = []
     return selected_ids
+
+def update_class(selected_class, class_dict):
+    white_buffer = 20 # --> see visual_utils.construct_grey_palette() 
+    selected_class_data = class_dict[selected_class]
+    n_colors = len(set(selected_class_data))
+    colors = visual_utils.construct_grey_palette(n_colors, white_buffer)
+    color_dict = visual_utils.create_color_dict(colors, selected_class_data)
+    return selected_class_data, color_dict
