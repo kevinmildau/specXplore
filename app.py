ee13d62f839d6e2a362cd3f717459b15c178f91d
Kevin Mildau <kevin.mildau@gmail.com>
2022-12-13 11:09:57 +0100

    egonet cythonized, style fixes

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ from utils import dashboard_components as _dc
 from utils import loading as load_utils
 from utils import visuals as visual_utils
 from utils import process_matchms as _myfun
-from utils import utils054
 from utils import egonet
 from utils import augmap
 from utils import tsne_plotting
 from utils import cytoscape_cluster
 from utils import fragmap
 from utils import parsing
+import cython_utils
 import pickle
 import seaborn as sns
 import copy
@@ -27,7 +27,7 @@ import dash_cytoscape as cyto
 import plotly.graph_objects as go
 from scipy.cluster import hierarchy
 
-app = Dash(__name__)
+app=Dash(__name__)
 
 # TODO: load necessary data.
 # --> pandas df with spec_id, x and y coord, and classification columns.
@@ -37,53 +37,56 @@ app = Dash(__name__)
 # --> spectrum list with list index corresponding to spec_id (for now)
 global STRUCTURE_DICT
 global CLASS_DICT
-STRUCTURE_DICT, CLASS_DICT = load_utils.process_structure_class_table(
+STRUCTURE_DICT, CLASS_DICT=load_utils.process_structure_class_table(
     "data/classification_table.csv")
 global AVAILABLE_CLASSES
-AVAILABLE_CLASSES = list(CLASS_DICT.keys())
+AVAILABLE_CLASSES=list(CLASS_DICT.keys())
 #print(AVAILABLE_CLASSES)
 
-# TODO: [ ] "Load" pairwise similarity matrices using numpy on disk indexing (need to be saved accordingly)
-# TODO: [ ] Create more dynamic data structures to allow between 1 and 5 similarity scores. 
-#       [ ] implement corresponding layering in heatmap visual and hover inf.
+
 global SM_MS2DEEPSCORE
 global SM_MODIFIED_COSINE
 global SM_SPEC2VEC
-SM_MS2DEEPSCORE, SM_MODIFIED_COSINE, SM_SPEC2VEC = load_utils.load_pairwise_sim_matrices()
+SM_MS2DEEPSCORE, SM_MODIFIED_COSINE, SM_SPEC2VEC=load_utils.load_pairwise_sim_matrices()
 
 global TSNE_DF
 with open("data/tsne_df.pickle", 'rb') as handle:
-    TSNE_DF = pickle.load(handle)
+    TSNE_DF=pickle.load(handle)
 
 # Initializing color dict
-selected_class_data = CLASS_DICT[AVAILABLE_CLASSES[0]]
+selected_class_data=CLASS_DICT[AVAILABLE_CLASSES[0]]
 # Create overall figure with color_dict mapping
-n_colors = len(set(selected_class_data)) # TODO: speed this up using n_clust argument that is pre-computed
-colors = visual_utils.construct_grey_palette(n_colors, white_buffer = 20)
-init_color_dict = visual_utils.create_color_dict(colors, selected_class_data)
+n_colors=len(set(selected_class_data)) # TODO: speed this up using n_clust argument that is pre-computed
+colors=visual_utils.construct_grey_palette(n_colors, white_buffer=20)
+init_color_dict=visual_utils.create_color_dict(colors, selected_class_data)
 
 global ALL_SPEC_IDS
-ALL_SPEC_IDS = TSNE_DF.index # <-- add list(np.unique(spec_id_list of sorts))
+ALL_SPEC_IDS=TSNE_DF.index # <-- add list(np.unique(spec_id_list of sorts))
 
 global ALL_SPECTRA
-file = open("data/cleaned_demo_data.pickle", 'rb')
-ALL_SPECTRA = pickle.load(file)
+file=open("data/cleaned_demo_data.pickle", 'rb')
+ALL_SPECTRA=pickle.load(file)
 file.close()
 
-app = dash.Dash(external_stylesheets=[dbc.themes.YETI]) # MORPH or YETI style.
-app.layout = \
-html.Div([
+# PROTOTYPING CYTHON DATA STRUCTURES
+SOURCE, TARGET, VALUE = cython_utils.construct_long_format_sim_table(SM_MS2DEEPSCORE)
+print(TSNE_DF)
+NODE_ID = np.array(TSNE_DF["id"], dtype=np.int32)
+NODE_X = np.array(TSNE_DF["x"], dtype=np.double)
+NODE_Y = np.array(TSNE_DF["y"], dtype=np.double)
+
+file = open("data/extracted_precursor_mz_values.pickle", 'rb')
+MZ = pickle.load(file) 
+file.close()
+
+app=dash.Dash(external_stylesheets=[dbc.themes.YETI]) # MORPH or YETI style.
+app.layout=html.Div([
     dbc.Row([
-        dbc.Col([
-            html.H1([
-                html.B("specXplore prototype")], 
-                    style={"margin-bottom": "-0.1em"})], 
-                    width=6)]),
+        dbc.Col([html.H1([html.B("specXplore prototype")], 
+            style={"margin-bottom": "-0.1em"})], width=6)]),
     dbc.Row([
-        dbc.Col([
-            html.H6(
-                html.H6("Authors: Kevin Mildau - Henry Ehlers"))], 
-                width=7),
+        dbc.Col(
+            [html.H6("Authors: Kevin Mildau - Henry Ehlers")], width=7),
         dbc.Col(
             dcc.Tabs(id="right-panel-tab-group", value='right-panel-tab', 
                 children=[
@@ -92,108 +95,67 @@ html.Div([
                     dcc.Tab(label='Augmap', value='tab-augmap'),
                     dcc.Tab(label='Settings', value='tab-settings'),
                     dcc.Tab(label='Data View', value='tab-data')]), 
-                width = 5)
+                width=5)
     ]),
     html.Br(),
     dbc.Row([
-        dbc.Col([
-            dcc.Graph(id = "tsne-overview-graph", 
-                figure={}, 
-                style={"width":"100%","height":"60vh", 
-                    "border":"1px grey solid"})], 
-            width=7),
-        dbc.Col([html.Div(id='right-panel-tabs-content')], 
-            width=5),
-    ], style = {"margin-bottom": "-1em"}),
+        dbc.Col([dcc.Graph(id="tsne-overview-graph", figure={}, 
+            style={"width":"100%","height":"60vh", 
+            "border":"1px grey solid"})], width=7),
+        dbc.Col([html.Div(id='right-panel-tabs-content')], width=5),
+    ], style={"margin-bottom": "-1em"}),
     html.Br(),
     dbc.Row([
-        dbc.Col([
-            html.H6("Selected Points for Cluster View:")], width = 6),
-        dbc.Col([
-            html.H6("Set Edge Threshold:")], width = 2),
-        dbc.Col([
-            dcc.Input(
-                id="threshold_text_input", type="number", debounce = True,
-                placeholder = "Threshold 0 < thr < 1, def. 0.9", 
-                style = {"width" : "100%"})], 
-            width = 4),
-    ]),
+        dbc.Col([html.H6("Selected Points for Cluster View:")], width=6),
+        dbc.Col([html.H6("Set Edge Threshold:")], width=2),
+        dbc.Col([dcc.Input( id="threshold_text_input", type="number", 
+            debounce=True, placeholder="Threshold 0 < thr < 1, def. 0.9", 
+            style={"width" : "100%"})], width=4)]),
     dbc.Row([
-        dbc.Col([
-            dcc.Dropdown(id='clust-dropdown' , 
-                multi=True, style={'width': '100%', 'font-size': "75%"}, 
-                options = ALL_SPEC_IDS)], 
-            width = 6),
+        dbc.Col([dcc.Dropdown(id='clust-dropdown', multi=True, 
+            style={'width': '100%', 'font-size': "75%"}, 
+            options=ALL_SPEC_IDS)], width=6),
         dbc.Col([html.H6("Selected Points for Focus View:")], width=2),
-        dbc.Col([
-            dcc.Dropdown(id='focus_dropdown' , 
-                multi=True, style={'width': '100%', 'font-size': "75%"}, 
-                options = ALL_SPEC_IDS)], 
-            width = 4),
-    ]),
+        dbc.Col([dcc.Dropdown(id='focus_dropdown', multi=True, 
+            style={'width': '100%', 'font-size': "75%"}, 
+            options=ALL_SPEC_IDS)], width=4)]),
     dbc.Row([
-        dbc.Col([html.Div( style={'width': '100%'})], width = 6),
+        dbc.Col([html.Div( style={'width': '100%'})], width=6),
         dbc.Col([html.H6("Reload open tab:")],width=4),
-        dbc.Col([
-            dbc.Button('Submit Reload', 
-                id='refresh-open-tab-button', 
-                style={"width":"100%"})], 
-            width = 2),
-    ]),
+        dbc.Col([dbc.Button('Submit Reload', id='refresh-open-tab-button', 
+            style={"width":"100%"})], width=2),]),
     dbc.Row([
-        dbc.Col([html.Div( style={'width': '100%'})], width = 6),
+        dbc.Col([html.Div( style={'width': '100%'})], width=6),
         dbc.Col([html.H6("Set expand level:")], width=4),
-        dbc.Col([
-            dcc.Input(
-                id="expand_level_input", type="number", debounce = True,
-                placeholder = "Value between 1 < exp.lvl. < 5, def. 1", 
-                style = {"width" : "100%"})], 
-            width = 2),
-    ]),
+        dbc.Col([dcc.Input( id="expand_level_input", type="number", 
+            debounce=True, 
+            placeholder="Value between 1 < exp.lvl. < 5, def. 1", 
+            style={"width" : "100%"})], width=2)]),
     html.Br(),
     dbc.Row([
-    dbc.Col([
-        dbc.Button(
-            "Generate Fragmap", id = "push_fragmap", style={"width":"100%"})], 
-        width=2),
-    dbc.Col([
-        dbc.Button("Generate Spectrum Plot", style={"width": "100%"})],
-        width=2),
-    dbc.Col([
-        dbc.Button("Show Spectrum Data", style={"width": "100%"})],
-        width=2),
-    dbc.Col([
-        dcc.Dropdown(
-            id='class-dropdown' , multi=False, clearable=False, 
-            options = AVAILABLE_CLASSES, value = AVAILABLE_CLASSES[0])], 
-        width = 4),
-    dbc.Col([
-        dbc.Button(
-            "Push Class Selection", id = "push-class", 
-            style={"width":"100%"})],
-        width=2)
-    ]),
-    dcc.Store(id = "edge_threshold", data = 0.9),
-    dcc.Store(id = "expand_level", data = int(1)),
-    dcc.Store(id = "selected_class_level", data = AVAILABLE_CLASSES[0]),
-    dcc.Store(id = "selected_class_data", 
-        data = CLASS_DICT[AVAILABLE_CLASSES[0]]),
-    dcc.Store(id = "color_dict",  data = init_color_dict),
+        dbc.Col([dbc.Button("Generate Fragmap", id="push_fragmap", 
+            style={"width":"100%"})], width=2),
+        dbc.Col([dbc.Button("Generate Spectrum Plot", 
+            style={"width": "100%"})], width=2),
+        dbc.Col([dbc.Button("Show Spectrum Data", style={"width": "100%"})],
+            width=2),
+        dbc.Col([dcc.Dropdown(id='class-dropdown' , multi=False, 
+            clearable=False, options=AVAILABLE_CLASSES, 
+            value=AVAILABLE_CLASSES[0])], width=4),
+    dbc.Col([dbc.Button("Push Class Selection", id="push-class", 
+        style={"width":"100%"})], width=2)]),
+    dcc.Store(id="edge_threshold", data=0.9),
+    dcc.Store(id="expand_level", data=int(1)),
+    dcc.Store(id="selected_class_level", data=AVAILABLE_CLASSES[0]),
+    dcc.Store(id="selected_class_data", data=CLASS_DICT[AVAILABLE_CLASSES[0]]),
+    dcc.Store(id="color_dict",  data=init_color_dict),
     html.Br(),
-    dbc.Row([
-        dbc.Col([
-            html.Div(id = "fragmap_panel", 
-                style={"width":"100%", "border":"1px grey solid"})], 
-            width=12)]
-    ),
+    dbc.Row([dbc.Col([html.Div(id="fragmap_panel", 
+        style={"width":"100%", "border":"1px grey solid"})], width=12)]),
     html.Br(),
-    dbc.Row([
-        dbc.Col([
-            html.Div(id = "data-panel", 
-                style={"width":"100%", "border":"1px grey solid"})], 
-            width=12)]
-    )
-], style = {"width" : "100%"})
+    dbc.Row([dbc.Col([html.Div(id="data-panel", 
+        style={"width":"100%", "border":"1px grey solid"})], width=12)])], 
+        style={"width" : "100%"})
 
 @app.callback([Output("edge_threshold", "data"),
                Output("threshold_text_input", "placeholder")],
@@ -201,7 +163,7 @@ html.Div([
               Input("threshold_text_input", "value")])
 
 def update_threshold_trigger_handler(n_submit, new_threshold):
-    new_threshold, new_placeholder = parsing.update_threshold(new_threshold)
+    new_threshold, new_placeholder=parsing.update_threshold(new_threshold)
     return new_threshold, new_placeholder
 
 @app.callback(
@@ -211,7 +173,7 @@ def update_threshold_trigger_handler(n_submit, new_threshold):
     Input("expand_level_input", "value"))
 
 def expand_trigger_handler(n_submit, new_expand_level):
-    new_expand_level, new_placeholder = parsing.update_expand_level(
+    new_expand_level, new_placeholder=parsing.update_expand_level(
         new_expand_level)
     return new_expand_level, new_placeholder
 
@@ -231,7 +193,7 @@ def left_panel_trigger_handler(
     selected_class_data, 
     color_dict):
     """ Modifies global overview plot in left panel """
-    tsne_fig = tsne_plotting.plot_tsne_overview(
+    tsne_fig=tsne_plotting.plot_tsne_overview(
         point_selection, selected_class_level, selected_class_data, TSNE_DF, 
         color_dict)
     return tsne_fig
@@ -245,7 +207,7 @@ def left_panel_trigger_handler(
     State("class-dropdown", "value"))
 def class_update_trigger_handler(n_clicks, selected_class):
     """ Wrapper Function that construct class dcc.store data. """
-    selected_class_data, color_dict = parsing.update_class(selected_class, 
+    selected_class_data, color_dict=parsing.update_class(selected_class, 
         CLASS_DICT)
     return selected_class, selected_class_data, color_dict
 
@@ -263,23 +225,24 @@ def right_panel_trigger_handler(
     tab, n_clicks, clust_selection, color_dict, selected_class_data, threshold, 
     expand_level):
     if tab == "tab-cluster" and clust_selection:
-        panel = cytoscape_cluster.generate_cluster_node_link_diagram(
+        panel=cytoscape_cluster.generate_cluster_node_link_diagram(
             TSNE_DF, clust_selection, SM_MS2DEEPSCORE, selected_class_data, 
             color_dict, threshold)
     elif tab == "tab-egonet"  and clust_selection:
-        panel = egonet.generate_egonet(
-            clust_selection, SM_MS2DEEPSCORE, TSNE_DF, threshold, expand_level)
+        panel = egonet.generate_egonet_cythonized(clust_selection, SOURCE, TARGET, VALUE, TSNE_DF, MZ, threshold, expand_level)
+        #panel=egonet.generate_egonet(
+        #    clust_selection, SM_MS2DEEPSCORE, TSNE_DF, threshold, expand_level)
     elif tab == "tab-augmap"  and clust_selection:
-        panel = augmap.generate_augmap(
+        panel=augmap.generate_augmap(
             clust_selection, SM_MS2DEEPSCORE, SM_MODIFIED_COSINE, SM_SPEC2VEC, 
             threshold)
     elif tab == "tab-settings":
-        panel = [html.H6("Settings panel inclusion pending.")]
+        panel=[html.H6("Settings panel inclusion pending.")]
     elif tab == "tab-data":
-        panel = [html.H6("Data panel inclusion pending.")]
+        panel=[html.H6("Data panel inclusion pending.")]
     else:
         warning("Nothing selected for display in right panel yet.")
-        panel = [html.H6("empty-right-panel")]
+        panel=[html.H6("empty-right-panel")]
     return panel
 
 # tsne-overview selection data trigger ----------------------------------------
@@ -288,18 +251,18 @@ def right_panel_trigger_handler(
     Input('tsne-overview-graph', 'selectedData'))
 def plotly_selected_data_trigger(plotly_selection_data):
     """ Wrapper Function for tsne point selection handling. """
-    selected_ids = parsing.extract_identifiers(plotly_selection_data)
+    selected_ids=parsing.extract_identifiers(plotly_selection_data)
     return selected_ids
 
 # Fragmap trigger -------------------------------------------------------------
 @app.callback(
     Output('fragmap_panel', 'children'),
     Input('push_fragmap', 'n_clicks'), # trigger only
-    State('focus_dropdown', 'value'))
+    State('clust-dropdown', 'value'))
 def fragmap_trigger(n_clicks, selection_data):
     """ Wrapper function that calls fragmap generation modules. """
     # Uses: global variable ALL_SPECTRA
-    fragmap_panel = fragmap.generate_fragmap(selection_data, ALL_SPECTRA)
+    fragmap_panel=fragmap.generate_fragmap_panel(selection_data, ALL_SPECTRA)
     return fragmap_panel
 
 if __name__ == '__main__':
---
+++ utils/cython_utils.pyx
@@ -0,0 +1,228 @@
+# distutils: language = c++
+# cython: c_string_type=unicode, c_string_encoding=utf8
+import cython
+from libcpp.string cimport string
+from libcpp.vector cimport vector
+import numpy as np
+from cython cimport boundscheck, wraparound
+import copy
+import plotly.express as px
+
+#@cython.boundscheck(False)
+#@cython.wraparound(False)
+def construct_long_format_sim_table(double[:,::1] sm):
+    """ Cython function which constructs index pair and value arrays for
+    pairwise similarity matrix such that each unique pair and value
+    combinaiton is available. """
+    assert sm.shape[0] == sm.shape[1]
+    cdef int n_nodes = int(sm.shape[0])
+    cdef int i
+    cdef long[:, ::1] index_array = np.ascontiguousarray(
+        np.vstack(np.triu_indices(n_nodes, k=1)).T)
+    cdef int n = index_array.shape[0]
+    cdef double[::1] value_array = np.zeros(n, dtype=np.double)
+    cdef long[::1] source_array = np.zeros(n, dtype=np.int_)
+    cdef long[::1] target_array = np.zeros(n, dtype=np.int_)
+    for i in range(0, n):
+        value_array[i] = sm[index_array[i][0]][index_array[i][1]]
+        source_array[i] = index_array[i][0]
+        target_array[i] = index_array[i][1]
+    return np.array(source_array), np.array(target_array), np.array(value_array)
+
+#@cython.boundscheck(False)
+#@cython.wraparound(False)
+def extract_selection(
+    long[:] source, long[:] target, double[:] value, long[:] selected_indexes, double threshold):
+    """ Cython function loops through similarity list and filters down to
+    selection set. """
+    #assert source.shape == target.shape == value.shape
+    cdef int nmax = int(source.shape[0])
+    cdef int i
+    cdef int k = 0
+    cdef set selected_set = set(selected_indexes)
+
+    cdef double[::1] out_value = np.zeros(nmax, dtype=np.double)
+    cdef long[::1] out_source = np.zeros(nmax, dtype=np.int_)
+    cdef long[::1] out_target = np.zeros(nmax, dtype=np.int_)
+
+    for i in range(0, nmax):
+        if source[i] in selected_set and target[i] in selected_set and value[i] > threshold:
+            out_value[k] = value[i]
+            out_source[k] = source[i]
+            out_target[k] = target[i]
+            k += 1
+    return np.array(out_value[0:k]), np.array(out_source[0:k]), np.array(out_target[0:k])
+
+#@cython.boundscheck(False)
+#@cython.wraparound(False)
+def extract_above_threshold(
+    long[:] source, long[:] target, double[:] value, double threshold):
+    """ Cython function loops through similarity list and filters down to
+    selection set. """
+    #assert source.shape == target.shape == value.shape
+    cdef int nmax = int(source.shape[0])
+    cdef int i
+    cdef int k = 0
+    cdef double[::1] out_value = np.zeros(nmax, dtype=np.double)
+    cdef long[::1] out_source = np.zeros(nmax, dtype=np.int_)
+    cdef long[::1] out_target = np.zeros(nmax, dtype=np.int_)
+    for i in range(0, nmax):
+        if value[i] > threshold:
+            out_value[k] = value[i]
+            out_source[k] = source[i]
+            out_target[k] = target[i]
+            k += 1
+    return np.array(out_value[0:k]), np.array(out_source[0:k]), np.array(out_target[0:k])
+
+#@cython.boundscheck(False)
+#@cython.wraparound(False)
+def creating_branching_dict_new(long[:] source, long[:] target, long root, long n_levels):
+    """Function creates edge branching edge lists."""
+    # PSEUDOCODE
+    # Start with root id
+    # Root is a node.
+    #   For root, find all edges that source or target it and add to edges_set.
+    #   In addition, make a set of all nodes that are connected to root. 
+    #   This will constitute level 1 of the branching network.
+    #   Before going into levels loop, check whether anything beyond root was
+    #   added.
+    # For lvl in n_levels:
+    #   Take note of the current edge and node set.
+    #   Sweep the edge list for any edges connected to the current node set.
+    #   Extract those edges and nodes, save as lvl_n sets, add lvl_n sets
+    #   to global sets.
+    #   Before going to next level, assess whether any new nodes were found. If
+    #   Not, abort.
+    # Input
+    #   Any pair in source and target will be above threshold and be a 
+    #   legitimate new edge if connected to any other nodes.
+    
+    # Code Notes --------------------------------------------------------------
+    # Edge ids can be simple index integers within this function. Each index
+    # points to the corresponding source and target entries.
+
+    #cdef vector[string] edge_ids = np.core.defchararray.add(
+    #    source.astype(np.str_), np.repeat(np.str_("-"), len(source)))
+    #edge_ids = np.core.defchararray.add(edge_ids, target.astype(np.str_))
+    cdef vector[int] edge_ids = np.arange(0, source.shape[0], dtype = np.integer)
+    cdef int i
+    cdef int j
+    cdef set all_nodes = set()
+    cdef set all_edges = set()
+    cdef int n_edges = source.shape[0]
+    cdef set tmp_nodes
+    cdef set tmp_edges
+    cdef long zero = int(0)
+    all_nodes.add(root)
+    cdef dict branching_dict = dict()
+
+    # Initialize Layer 1
+    tmp_nodes = set()
+    tmp_edges = set()
+    for j in range(zero, n_edges):
+        if source[j] == root or target[j] == root:
+            tmp_nodes.add(source[j])
+            tmp_nodes.add(target[j])
+            tmp_edges.add(edge_ids[j]) # or simply j
+    if len(tmp_edges) != 0:
+        branching_dict[0] = {"nodes": [root], "edges": list(tmp_edges)}
+    else:
+        branching_dict[0] = {"nodes": [root], "edges": []}
+    all_edges = all_edges.union(tmp_edges)
+    all_nodes = all_nodes.union(tmp_nodes)
+
+    # Populate Levels
+    for i in range(1, n_levels):
+        if len(tmp_nodes) == zero:
+            print((f"Stopping edge tracing at level:{i}."
+                "No new nodes to expand from"))
+            break
+        current_level_nodes = list(tmp_nodes)
+        tmp_nodes = set()
+        tmp_edges = set()
+        # This finds level i edges, and level i+1 nodes
+        for j in range(0, n_edges):
+            # if the edge connects to any previous nodes, but itself edge_id isn't captured yet.
+            if (source[j] in all_nodes or target[j] in all_nodes) and not (edge_ids[j] in all_edges): # BEWARE OF LONG AND INT TYPING!
+                if not source[j] in all_nodes:
+                    tmp_nodes.add(source[j])
+                if not target[j] in all_nodes:
+                    tmp_nodes.add(target[j])
+                tmp_edges.add(edge_ids[j]) # or simply j
+        
+        if len(tmp_edges) != zero:
+            all_edges = all_edges.union(tmp_edges)
+            all_nodes = all_nodes.union(tmp_nodes)
+            branching_dict[i] = {
+                "nodes": list(current_level_nodes), "edges": list(tmp_edges)}
+        else:
+            branching_dict[i] = {
+                "nodes": list(current_level_nodes), "edges": list()}
+            print(f"Stopping edge tracing at level:{i}. No new edges found.")
+            break
+
+        branching_dict[i] = {
+            "nodes": list(current_level_nodes), "edges": list(tmp_edges)}
+        all_edges = all_edges.union(tmp_edges)
+        all_nodes = all_nodes.union(tmp_nodes)
+    
+    # Add nodes added in final level if any
+    if len(tmp_nodes) != 0:
+        branching_dict[n_levels+1] = {"nodes": list(tmp_nodes), "edges": []}
+    return branching_dict
+
+
+def generate_edge_elements_and_styles(
+    branching_dict, long[:] source, long[:] target, nodes):
+    """Generates an edge list and style list for a given branching dict. """
+    n_colors = max(2, len(branching_dict) + 1)
+    # "ice" might be a good alternative color
+    colors = px.colors.sample_colorscale(
+        "Viridis", [n/(n_colors -1) for n in range(n_colors)])
+    opacities = np.arange(0.8, 0.4, step = -(0.8 - 0.4) / n_colors)
+    # widths = np.arange(5, 0.5, step = -(5 - 0.5) / n_colors)
+    cdef long idx, key, edge
+    cdef long node_count = 0
+    cdef long node_counter = 0
+    cdef long edge_count = 0
+    cdef long edge_counter = 0
+    cdef long max_edges = 400
+    cdef long limit_count = 0
+    for idx, key in enumerate(branching_dict):
+        edge_count += len(branching_dict[key]['edges'])
+        node_count += len(branching_dict[key]['nodes'])
+    
+    edge_elems = [{}] * min(edge_count, max_edges)
+    styles = [{}] * (len(branching_dict.keys()))
+    
+    limit = False
+    if edge_count > max_edges:
+        print("Branching tree involves more than 200 edges.", 
+            "Edges only shown for first level to avoid dash-cytoscape overload.")
+        limit = True
+        
+
+    for idx, key in enumerate(branching_dict):
+        styles[idx] = {
+            "selector":'.{}'.format(key), 
+            "style":{
+                "line-color":colors[idx], 
+                'opacity':opacities[idx], 
+                #'width':widths[idx],
+                "background-color":colors[idx], 
+                'border-width':'2', 
+                'border-color':'black'}}
+        if not limit or idx < 1:
+            for edge in branching_dict[key]["edges"]:
+                if limit_count < max_edges:
+                    edge_elems[edge_counter] = {'data': {
+                            'id':str(100000 + edge), 
+                            'source':str(source[edge]) ,
+                            'target':str(target[edge])},
+                            'classes':str(key)}
+                    edge_counter += 1
+                    limit_count += 1
+        for node in branching_dict[key]["nodes"]:
+            nodes[node]['classes'] = str(key)
+    #print("FROM CYTHON", styles)
+    return (edge_elems, styles)
