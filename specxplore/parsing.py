e7d528baec8c6aa4659059de60fa7996ad4c8d1a
Kevin Mildau <kevin.mildau@gmail.com>
2022-12-26 10:55:03 +0100

    added documentation

--- specxplore/parsing.py
+++ specxplore/parsing.py
@@ -1,19 +1,65 @@
-from utils import visuals as visual_utils
+# Developer Notes:
+# elem["customdata"][0] is a strong input assumption that coulb be made
+# clearer.
+
+# white buffer should be a provided setting: white_buffer = 20
+# black buffer should be an input, and a provided setting.
+
+# visual utils and the functions in this could be merged into a more general 
+# utils module or into more concrete intermediate processing modules.
+
+# expand level limiters should be modifiable settings.
+
+
+from specxplore import visuals as visual_utils
 
 def extract_identifiers(plotly_selection_data):
     """ Function extracts custom_data id's from a provided point selection 
-        dictionary. """
+    dictionary. 
+        
+    Args / Parameters
+    ------
+    plotly_selection_data:
+        Selection data as returned by plotly (json format). spec_ids are 
+        assumed too be stored inside the 'customdata' component of the 
+        selection data.
+
+    Returns
+    ------
+    output:
+        List of spec_ids corresponding to the selected scatter points.
+    
+    """
     if plotly_selection_data != None:
         selected_ids = [
-            elem["customdata"][0] 
-            for elem 
-            in plotly_selection_data["points"]]
-        print("slected _ids:", selected_ids)
+            elem["customdata"][0] for elem in plotly_selection_data["points"]]
     else:
         selected_ids = []
     return selected_ids
 
 def update_class(selected_class, class_dict):
+    """ Function updates selected_class_data and color_dict elements in line
+    with provided selected_class.
+        
+    Args
+    ------
+    selected_class:
+        String identifying the selected class. The string should be a key in 
+        class_dict.
+    class_dict:
+        A dictionary with class_string keys and corresponding class assignment
+        list of each spec_id: {class_string : class_assignment_list}. The class
+        assignment list is ordered to correspond to spec_id idx ordering.
+
+    Returns
+    ------
+    output 0:
+        selected_class_data, a list of class assignments for each spec_id 
+        in original idx ordering.
+    output 1:
+        A color dictionary where each unique cluster is a string key with the
+        corresponding value being a hex code color: {clust_key : color_string}.
+    """
     white_buffer = 20 # --> see visual_utils.construct_grey_palette() 
     selected_class_data = class_dict[selected_class]
     n_colors = len(set(selected_class_data))
@@ -22,24 +68,66 @@ def update_class(selected_class, class_dict):
     return selected_class_data, color_dict
 
 def update_expand_level(new_expand_level):
+    """ Function updates expand level and placeholder in line with input.
+
+    Expand levels define the number of branching out events for the 
+    ego-network represenation. The default value is 1, for all direct 
+    connections to the root / ego-node. Large values or values below 1 are
+    ill suited for this representation and automatically rejected. 
+    A value of 0 would lead to no connections and defeat the point of the
+    visualization. Negative values make no sense. Large values will connect
+    increasingly irrelevant spectra, or lead to complete graphs depending on
+    threshold settings. Sensible defaults are [1 6].
+    
+    Args / Parameters
+    ------
+    new_expand_level: 
+        New expand level; integer >=1 and should be a small number in line
+        with lower and upper limit settings.
+
+    Returns
+    ------
+    output 0:
+        A new expand level setting as integer.
+    output 1:
+        A new placeholder text for expand level text input box.
+    """
+    lower_limit = 1
+    upper_limit = 6
     if (new_expand_level 
-        and new_expand_level >= 1 
-        and new_expand_level <= 6 
+        and new_expand_level >= lower_limit 
+        and new_expand_level <= upper_limit
         and isinstance(new_expand_level, int)):
-        new_placeholder = ('Expand Level 1 =< thr <= 6,'
+        new_placeholder = (
+            f'Expand Level {lower_limit} =< thr <= {upper_limit},'
             f'current: {new_expand_level}')
         return new_expand_level, new_placeholder
     else:
         default_expand_level = 1
-        default_placeholder = "Expand Level 1 =< thr <= 6, def. 1"
+        default_placeholder = (
+            f'Expand Level {lower_limit} =< thr <= {upper_limit},')
         return default_expand_level,  default_placeholder
 
 def update_threshold(new_threshold):
+    """ Function updates threshold level and placeholder in line with input.
+    
+    Args / Parameters
+    ------
+    new_threshold: 
+        New threshold; float < 1 and > 0.
+
+    Returns
+    ------
+    output 0:
+        A new threshold setting, float.
+    output 1:
+        A new placeholder text for threshold text input box.
+    """
     if (new_threshold 
-        and new_threshold < 1 
-        and new_threshold > 0
+        and new_threshold < 1 and new_threshold > 0
         and isinstance(new_threshold, float)):
-        new_placeholder = ('Threshold 0 < thr < 1,' 
+        new_placeholder = (
+            'Threshold 0 < thr < 1,' 
             f'current: {new_threshold}')
         return new_threshold, new_placeholder
     else:
--- specxplore/tsne_plotting.py
+++ specxplore/tsne_plotting.py
@@ -1,26 +1,63 @@
-import plotly.express as px
+# Developer Notes
+# for more complex plot specifications, use go.Scattergl() rather than px
 
+# Settings
+# {tsne_overview : 
+# {highlight_color : "#FF10F0",  paper_bgcolor :'#feeff4', plot_bgcolor='#feeff4'}}
+
+import plotly.express as px
 def plot_tsne_overview(
     point_selection, selected_class_level, selected_class_data, tsne_df, 
     color_dict):
+    """Constructs the t-SNE overview graph plotly figure object.
+    
+    Args / Parameters
+    ------
+    point_selection:
+        None, a single selected spec_id, or a multi-selection of spec_ids. The
+        selection is used to highlight the cluster to which a selected spec_id
+        belongs. If a multi-selection is provided, the first indexed spec_id
+        is used for cluster highlighting.
+    selected_class_level: 
+        Name string of the selected class. 
+    selected_class_data:
+        The list of class assignments for the selected class level.
+    tsne_df:
+        Dataframe with t-SNE coordinates for each spec_id.
+    color_dict:
+        A color dictionary where each unique cluster is a string key with the
+        corresponding value being a hex code color: {clust_key : color_string}.
+    
+    Returns
+    ------
+    fig:
+        t-SNE overview graph as ploty figure object.
+    """
+    # Modify color dict to highlight class of selected spectrum; if multiple
+    # selected the first indexed spectrum is highlighted.
     if point_selection:  
-        selected_point = point_selection["points"][0]["customdata"][0]
-        color_dict[selected_class_data[selected_point]] = "#FF10F0"
-    tsne_df_tmp = tsne_df
-    tsne_df_tmp["clust"] = selected_class_data
-    fig = px.scatter(
-        tsne_df_tmp, x = "x", y = "y", color = "clust", custom_data=["id"], 
-        color_discrete_map=color_dict, render_mode='webgl')
-    # for more complex specifications, use go.Scattergl()
-    fig.update_layout(clickmode='event+select', 
-                        margin = {"autoexpand":True, "b" : 0, "l":0, 
-                                "r":50, "t":0})
-    fig.update_layout(yaxis_visible=False, yaxis_showticklabels=False, 
+        selected_point=point_selection["points"][0]["customdata"][0]
+        color_dict[selected_class_data[selected_point]]="#FF10F0"
+    # Extend df to contain selected class data (always given)
+    tsne_df_tmp=tsne_df
+    tsne_df_tmp["clust"]=selected_class_data
+    fig=px.scatter(
+        tsne_df_tmp, 
+        x="x", y="y", color="clust", 
+        custom_data=["id"], 
+        color_discrete_map=color_dict, 
+        render_mode='webgl')
+    fig.update_layout(
+        clickmode='event+select', 
+        margin={"autoexpand":True, "b" : 0, "l":0, "r":50, "t":0})
+    fig.update_layout(
+        yaxis_visible=False, yaxis_showticklabels=False, 
         xaxis_visible=False, xaxis_showticklabels=False, 
-        title_text='T-SNE Overview', title_x=0.01, title_y=0.01,
-        legend_title_text = selected_class_level,
-        paper_bgcolor='#feeff4',
-        plot_bgcolor='#feeff4', uirevision = False,
-        legend = dict(title_font_family='Ubuntu',font=dict(size=6))
-        )
+        title_text='T-SNE Spectral Data Overview', 
+        title_x=0.01, title_y=0.01,
+        legend_title_text=selected_class_level,
+        paper_bgcolor='#feeff4', # black or white interfere with grayscale
+        plot_bgcolor='#feeff4', # black or white interfere with grayscale
+        uirevision=False, # prevent zoom level changes upon plot update
+        legend=dict(title_font_family='Ubuntu',font=dict(size=6)))
     return fig
