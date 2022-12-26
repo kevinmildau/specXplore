cefc4cba830075597bd48b91bd2c4acdb7758cfd
Kevin Mildau <kevin.mildau@gmail.com>
2022-12-26 10:56:43 +0100

    extended example

--- example_01_gnps_nih-dataset.py
+++ example_01_gnps_nih-dataset.py
@@ -1,6 +1,18 @@
+# conda activate ms2query 
+# conda env ms2query contains installs of ms2query and ms2deepscore with compatible
+# matchms verisons.
 from specxplore import importing
 
 spectra = importing.load_matchms_spectrum_objects_from_file(
     "data-input/GNPS-NIH-NATURALPRODUCTSLIBRARY.mgf")
+print("Spectra:", spectra[0:3], len(spectra))
+spectra_cleaned = importing.normalize_and_filter_peaks_multiple_spectra(spectra)
 
-print(spectra[1:3])
+print("Cleaned:", spectra_cleaned[0:3], len(spectra_cleaned))
+
+spectra_charge_fixed = importing.add_unknown_charges_to_spectra(
+    spectra_cleaned)
+
+print("Charges fixed:", spectra_charge_fixed[0:3], len(spectra_charge_fixed))
+
+
