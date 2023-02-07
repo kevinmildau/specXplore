32e82020c672aa4990e7a1aad5f5b3706d28f471
Kevin Mildau <kevin.mildau@gmail.com>
2023-02-07 09:56:01 +0100

    full refactor + drafting notes for multiplot

--- specxplore/spectrum_plot.py
+++ specxplore/spectrum_plot.py
@@ -1,133 +1,95 @@
-# Imports --------------------------------------------------------------------------------------------------------------
 import plotly.graph_objects as go
-import plotly.express as px
+from plotly.subplots import make_subplots
 import pandas as pd
 import numpy as np
-import matchms
-import pickle
+from typing import List
+import specxplore.specxplore_data
+from specxplore.specxplore_data import Spectrum
 
+PLOT_LAYOUT_SETTINGS = {
+        'template':"simple_white",
+        'xaxis':dict(title="Mass to charge ratio"),
+        'yaxis':dict(title="Intensity", fixedrange=True),
+        'hovermode':"x",
+        'showlegend':True}
 
-# Functions -----------------------------------------------------------------------------------------------------------
-
-
-# Conversion of matchms objects to pandas
-def spectrum_list_to_pandas(id_list: [int], spec_list: [matchms.Spectrum]) -> pd.DataFrame:
-
-    # Ensure the provided ID list is valid
-    assert all([ind in range(0, len(spec_list)) for ind in id_list]), \
-        f"Error: Provided ID list {id_list} contains ID's which do not match the spec_list of length {len(spec_list)}."
-
-    # Initialize empty list of data frames
-    spec_data = list()
-
-    # Iterate over all indices specified or all indices in the data
-    for identifier in id_list:
-
-        # Extra data, create pandas data frame, and append to growing list data frames
-        spec_data.append(pd.DataFrame({"spectrum": identifier,
-                                       "m/z": spec_list[identifier].mz,
-                                       "intensity": spec_list[identifier].intensities}))
-
-    # Return single long data frame
-    return pd.concat(objs=spec_data, ignore_index=True)
-
-
-# Bar-plot of spectrum
-def spectrum_plot(id: int, spec_list: [matchms.Spectrum]) -> go.Figure:
-
-    # Subset selected ID and convert to pandas
-    spectrum = spectrum_list_to_pandas(id_list=[id], spec_list=spec_list)
-
-    # Create Empty Figure
-    figure = go.Figure(data=bar_hover_trace(spectrum["m/z"], spectrum["intensity"]))
-
-    # Set X and Y Axes Scales
+def generate_single_spectrum_plot(spectrum : Spectrum) -> go.Figure:
+    """ Generates single spectrum plot. """
+    hover_trace_invisible = bar_hover_trace(spectrum.mass_to_charge_ratios, spectrum.intensities, spectrum.identifier)
+    visual_trace = bar_line_trace(spectrum.mass_to_charge_ratios, spectrum.intensities)
+    figure = go.Figure(data = [hover_trace_invisible] + visual_trace)
     figure.update_yaxes(range=[0, 1])
-
-    # Add each bar of the bar-plot as a line shape
-    figure.update_layout(shapes=bar_shape_trace(spectrum["m/z"], spectrum["intensity"]))
-
-    # Format Layout
-    figure.update_layout(
-        template="simple_white",
-        xaxis=dict(title="m/z"),
-        yaxis=dict(title="Intensity", fixedrange=True),
-        hovermode="x",
-        showlegend=False
-    )
-
-    # Return
+    #visual_trace = bar_shape_trace(spectrum.mass_to_charge_ratios, spectrum.intensities)
+    #figure.update_layout(shapes=visual_trace)
+    figure.update_layout(**PLOT_LAYOUT_SETTINGS)
     return figure
 
-
-def bar_shape_trace(x_values: [float], y_values: [float]) -> [go.layout.Shape]:
+def bar_shape_trace(x_values: np.ndarray, y_values: np.ndarray) -> [go.layout.Shape]:
     kwargs = {'type': 'line', 'xref': 'x', 'yref': 'y', 'line_color': "black", 'line_width': 1, 'layer': "below",
               'opacity': 1}
     shapes = [go.layout.Shape(x0=x, y0=0, x1=x, y1=y, **kwargs) for x, y in zip(x_values, y_values)]
     return shapes
 
+def bar_line_trace(x_values: np.ndarray, y_values: np.ndarray) -> List[go.Scatter]:
+    kwargs = {
+        'mode': 'lines', 'opacity': 1, 'hoverinfo': "skip", 
+        'name' : '', 'showlegend':False}
+    lines_scatters = [ go.Scatter(x = [x,x], y =  [0,y], meta=np.abs(y), marker={'color': "black"}, **kwargs) 
+    for x,y in zip(x_values, y_values)]
+    return lines_scatters
 
-def bar_hover_trace(x_values: [float], y_values: [float]) -> go.Scatter:
-    kwargs = {'mode': 'markers', 'opacity': 0, 'hovertemplate': "%{meta:.4f}<extra></extra>"}
-    return go.Scatter(x=x_values, y=y_values, meta=y_values.abs(), marker={'color': "black"}, **kwargs)
-
+def bar_hover_trace(x_values: np.ndarray, y_values: np.ndarray, spectrum_identifier : int) -> go.Scatter:
+    kwargs = {
+        'mode': 'markers', 'opacity': 0, 'hovertemplate': "%{meta:.4f}<extra></extra>", 
+        'name' : f'Spectrum ID = {spectrum_identifier}'}
+    return go.Scatter(x=x_values, y=y_values, meta= np.abs(y_values), marker={'color': "black"}, **kwargs)
 
 # Mirror plot of two spectra
-def mirror_plot(id_a: int, id_b: int, spec_list: [matchms.Spectrum]) -> go.Figure:
-
-    # Subset selected ID's and convert to pandas
-    spectrum_a = spectrum_list_to_pandas(id_list=[id_a], spec_list=spec_list)
-    spectrum_b = spectrum_list_to_pandas(id_list=[id_b], spec_list=spec_list)
-
-    # Create Mirror Plot
-    figure = go.Figure(
-        data=[
-            bar_hover_trace(spectrum_a["m/z"], spectrum_a["intensity"]),
-            bar_hover_trace(spectrum_b["m/z"], spectrum_b["intensity"] * (-1.0))
-        ]
-    )
-
-    #
-    # Add each bar of the bar-plot as a line shape
-    figure.update_layout(shapes=bar_shape_trace(spectrum_a["m/z"], spectrum_a["intensity"]) +
-                                bar_shape_trace(spectrum_b["m/z"], spectrum_b["intensity"] * (-1.0))
-                         )
-
-    # Ensure the axes are scaled properly
+def generate_mirror_plot(top_spectrum : Spectrum, bottom_spectrum: Spectrum) -> go.Figure:
+    """ Generates spectrum mirrorplot. """
+    top_hover_trace_invisible = bar_hover_trace(
+        top_spectrum.mass_to_charge_ratios, top_spectrum.intensities, top_spectrum.identifier)
+    bottom_hover_trace_invisible = bar_hover_trace(
+        bottom_spectrum.mass_to_charge_ratios, bottom_spectrum.intensities * -1.0, bottom_spectrum.identifier)
+    top_visible_trace = bar_line_trace(top_spectrum.mass_to_charge_ratios, top_spectrum.intensities)
+    bottom_visible_trace = bar_line_trace(
+        bottom_spectrum.mass_to_charge_ratios, bottom_spectrum.intensities * -1.0)
+    data = [top_hover_trace_invisible, bottom_hover_trace_invisible] + top_visible_trace + bottom_visible_trace
+    figure = go.Figure(data = data)
     figure.update_yaxes(range=[-1, 1])
-
-    # Add reference y-axis (at y=0)
-    figure.add_hline(
-        y=0.0,
-        line_width=1,
-        line_color="black",
-        opacity=1
-    )
-
-    # Format Layout
-    figure.update_layout(
-        template="simple_white",
-        xaxis=dict(title="m/z"),
-        yaxis=dict(title="Intensity", fixedrange=True),
-        hovermode="x",
-        showlegend=False
-    )
-
-    # Return
+    figure.add_hline(y=0.0, line_width=1, line_color="black", opacity=1)
+    figure.update_layout(**PLOT_LAYOUT_SETTINGS)
     return figure
 
+def generate_multiple_spectra_plot(spectra : List[Spectrum]):
+    # This plot requires the concatenation of all kinds of traces; this means that the layout information
+    # for each subplot is effectively lost, affecting numerous plotting visual aspects
+    # Plotly's organization of the different trace layout attributes is absolute cancer
+    # the first trace has normal layout naming and can easily be set
+    # all subsequent traces have the layout names modified to be in line with index, e.g. y_axis_title2 and so on.
+    # currently the y limitations are also messed. 
+    # the legend is absolutely useless and sould be removed; this is because selecting a specific legend item is possible,
+    # however, the legend items are controlling invisible components....
+    # x axis do not need to be aligned, y axes however should always be the same.
+    # Loops
+    # for each spectrum: generate single_spectrum_plot
+    n_plots = len(spectra)
+    #figure = make_subplots(rows= int(np.ceil(n_plots / 2)), cols=2)
+    figure = make_subplots(
+        rows= int(n_plots), cols=1, 
+        subplot_titles=[f'Spectrum ID = {spectrum.identifier}' for spectrum in spectra])
+    
+    for idx, spectrum in enumerate(spectra):
+        tmp_spectrum_figure = generate_single_spectrum_plot(spectrum)
+        for trace in tmp_spectrum_figure.data:
+            figure.add_trace(trace, row = idx +1, col = 1)
+    
+    # TODO: ADD LOOP TO MODIFY INDEXED Y AXIS LIMITS AND TITLES
+    # TODO: ADD SINGLE LINE TO MODIFY X AXIS TITLE FOR THE LAST PLOT IN LINE. (OR LOOP FOR ALL PLOTS IF NCOL > 1)
+    return figure
 
-# Main ----------------------------------------------------------------------------------------------------------------
-
-# Load Original Spectra
-file = open("./data/0009_spectrums_difficult_1.pickle", 'rb')
-spectra = list(pickle.load(file))
-file.close()
-
-# Create and display spectrum plot
-single_spectrum_figure = spectrum_plot(id=1, spec_list=spectra)
-single_spectrum_figure.show()
-
-# Create and display mirror spectrum plot
-mirror_plot_figure = mirror_plot(id_a=3, id_b=4, spec_list=spectra)
-mirror_plot_figure.show()
+def generate_spectrum_plot_panel(identifiers : [int]):
+    ...
+    #if many do many
+    #if 2 do mirror
+    #if single do single
