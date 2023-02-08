6cfa8c3c7a5ef49e6e79ff390bf47c9c60173589
Kevin Mildau <kevin.mildau@gmail.com>
2023-02-08 10:31:44 +0100

    changed test to use custom spectrum dataclass

--- test_fragmap.py
+++ test_fragmap.py
@@ -1,15 +1,18 @@
 import pickle
 import specxplore.specxplore_data
-from specxplore.specxplore_data import specxplore_data
+from specxplore.specxplore_data import specxplore_data, Spectrum
 import numpy as np
 from specxplore import fragmap
 
 with open("data_import_testing/results/phophe_specxplore.pickle", 'rb') as handle:
-    data = pickle.load(handle)
+    data = pickle.load(handle).spectra
 
-spectra = data.spectra
+spectra = [Spectrum(spec.peaks.mz, max(spec.peaks.mz),idx, spec.peaks.intensities) for idx, spec in enumerate(data)]
+
+print(spectra[0])
 print(len(spectra))
-bins = [round(x, 1) for x in list(np.arange(0, 1000 + 0.1, 0.1))]
-fig = fragmap.generate_fragmap([0,16,17,18,19], spectra, 0.01, 1, 0, 1000, bins, 0, 200)
 
-fig.show(renderer = "browser")
+#bins = [round(x, 1) for x in list(np.arange(0, 1000 + 0.1, 0.1))]
+#fig = fragmap.generate_fragmap([0,16,17,18,19], spectra, 0.01, 1, 0, 1000, bins, 0, 200)
+
+#fig.show(renderer = "browser")
