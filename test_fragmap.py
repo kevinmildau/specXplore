aa8ecdda9b11d805fcfa16c62f847ceac7260f3c
Kevin Mildau <kevin.mildau@gmail.com>
2023-02-08 16:55:42 +0100

    added limited tests for partiall refactored fragmap

--- test_fragmap.py
+++ test_fragmap.py
@@ -12,6 +12,12 @@ spectra = [Spectrum(spec.peaks.mz, max(spec.peaks.mz),idx, spec.peaks.intensitie
 print(spectra[0])
 print(len(spectra))
 
+spectrum_df = fragmap.spectrum_list_to_pandas(spectra[0:2])
+print("--> Spectrum df", spectrum_df)
+spectrum_bin_template = [round(x, 1) for x in list(np.arange(0, 1000 + 1, 1))]
+print("--> Spectrum bin template head:", spectrum_bin_template[0:6])
+spectrum_df_binned = fragmap.bin_spectra(spectrum_df, spectrum_bin_template)
+print("--> Spectrum df after binning:", spectrum_df_binned)
 #bins = [round(x, 1) for x in list(np.arange(0, 1000 + 0.1, 0.1))]
 #fig = fragmap.generate_fragmap([0,16,17,18,19], spectra, 0.01, 1, 0, 1000, bins, 0, 200)
 
