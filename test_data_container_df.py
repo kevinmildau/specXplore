246287d0b0f017bdaf8ec47212694751ded4c2de
Kevin Mildau <kevin.mildau@gmail.com>
2023-02-14 16:55:41 +0100

    elaborated data container tests

--- test_data_container_df.py
+++ test_data_container_df.py
@@ -1,4 +1,4 @@
-from specxplore.specxplore_data import MultiSpectrumDataFrameContainer
+from specxplore.specxplore_data import SpectraDF
 import numpy as np
 import pandas as pd
 from collections import namedtuple
@@ -13,16 +13,24 @@ print(dict(zip(("identifier", "mass-to-charge-ratio", "intensity"), [np.int64, n
 
 
 
-expected_column_types_class = namedtuple("expected_column_types", ['id', 'mz', 'int'])
-expected_column_types = expected_column_types_class(id=np.int64, mz=np.double, int = np.double)
-print(expected_column_types)
-
-print(MultiSpectrumDataFrameContainer(df))
-newdata = MultiSpectrumDataFrameContainer(df)
-newdata.data["identifier"] = ["1", "2", "3"]
-print(newdata.data)
+#expected_column_types_class = namedtuple("expected_column_types", ['id', 'mz', 'int'])
+#expected_column_types = expected_column_types_class(id=np.int64, mz=np.double, int = np.double)
+#print(expected_column_types)
+print("IGNORING THE _data INTERNAL VARIABLE INDICATOR CAN LEAD TO NONSENSE SIDE EFFECTS IN THE OBJECT.")
+print("HOWEVER, THE GETTER APPROACH IS THE EASIEST WAY TO GET IMMUTABLE FEEL & ALERT PEOPLE TO NOT MODIFY PAST INIT.")
+print(SpectraDF(df))
+newdata = SpectraDF(df)
+newdata._data["identifier"] = ["a", "bunch of", "nonsense"] # <-- THIS OPERATION WORKS, BUT IT IS CONSIDERED BAD PRACTICE TO MODIFY INTERNAL CLASS OBJECTS!
+print(newdata._data)
 print(newdata.get_column_as_np("identifier"))
 print(newdata.get_data())
 
+print("USING GETTERS AVOIDS MUTABILITY BOTTLENECKS AND OVERWRITING PANDAS DF COLUMNS")
+df = pd.DataFrame({"intensity" : intensity, "mass-to-charge-ratio" : mz, "identifier" : identifier})
+newdata = SpectraDF(df)
+data = newdata.get_data()
+data["identifier"] = ["a", "bunch of", "nonsense"]
+print(data)
+print(newdata)
 # This line breaks... but the previous string replacement does not lead to any errors.
 #newdata.data = df
