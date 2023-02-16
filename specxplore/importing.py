1cfc22f87f60f3b8b7dbd7dcb7559b4d323f3bd2
Kevin Mildau <kevin.mildau@gmail.com>
2023-02-16 12:05:29 +0100

    added distance computation function

--- specxplore/importing.py
+++ specxplore/importing.py
@@ -368,3 +368,12 @@ def extract_similarity_scores_from_matchms_cosine_array(tuple_array : np.ndarray
         for elem in row:
             sim_data.append(float(elem[0])) # <- TODO: check float conversion necessary?
     return(np.array(sim_data).reshape(tuple_array.shape[0], tuple_array.shape[1]))
+
+def convert_similarity_to_distance(similarity_matrix : np.ndarray) -> np.ndarray:
+    """ 
+    Converts pairwise similarity matrix to distance matrix with values between 0 and 1 
+    """
+    distance_matrix = 1.- similarity_matrix
+    distance_matrix = np.round(distance_matrix, 6) # Round to deal with floating point issues
+    distance_matrix = np.clip(distance_matrix, a_min = 0, a_max = 1) # Clip to deal with floating point issues
+    return distance_matrix
