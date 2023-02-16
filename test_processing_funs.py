6cdc99e31757d83f69e32826271de9eb188fdd11
Kevin Mildau <kevin.mildau@gmail.com>
2023-02-16 11:35:35 +0100

    extended test case

--- test_processing_funs.py
+++ test_processing_funs.py
@@ -1,6 +1,6 @@
 from specxplore import importing
 import matchms
-
+import pandas as pd
 spectra_list_raw = list(matchms.importing.load_from_mgf(
     source = "data_import_testing/data/reference_standards_pos/dummy.mgf"))
 
@@ -15,7 +15,15 @@ for spectrum in spectra_list:
         missing_structures += 1
 print(f"Missing number of structure assignments: {missing_structures}")
 
-spectra_list = spectra_list[0:2]
+spectra_list = spectra_list[0:3]
 
-classification_df = importing.batch_run_get_classes(spectra_list) # GNPS API Based, Takes a whilte to Run 2 seconds per spectrum at least
-print(classification_df)
+if False: # prevent time consuming batch run is testing.
+    print(importing.get_classes(spectra_list[0].get("inchi"))) # single api call
+    classification_df = importing.batch_run_get_classes(spectra_list) # GNPS API Based, Takes a whilte to Run 2 seconds per spectrum at least
+    print(classification_df)
+    classification_df.to_csv("tmp_test_processing_funs_classifications.csv")
+else:
+    print(importing.get_classes(spectra_list[0].get("inchi")))
+    classification_dd = pd.read_csv("tmp_test_processing_funs_classifications.csv")
+
+print(importing.get_classes(None))
