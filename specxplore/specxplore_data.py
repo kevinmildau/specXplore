1700b20b782f983385ee36e61afce4ba4b58da79
Kevin Mildau <kevin.mildau@gmail.com>
2023-03-08 15:42:14 +0100

    minor editing

--- specxplore/specxplore_data.py
+++ specxplore/specxplore_data.py
@@ -9,18 +9,12 @@ import copy
 class specxplore_data:
   def __init__(
     self, ms2deepscore_sim, spec2vec_sim, cosine_sim, 
-    tsne_df, class_table, clust_table, is_standard, spectra, mz, specxplore_id
+    tsne_df, class_table, is_standard, spectra, mz, specxplore_id
     ):
     self.ms2deepscore_sim = ms2deepscore_sim
     self.spec2vec_sim = spec2vec_sim
     self.cosine_sim = cosine_sim
     self.tsne_df = tsne_df
-    #tmp_class_table = class_table.merge(
-    #  clust_table, 
-    #  how = 'inner', 
-    #  on='specxplore_id').drop(["specxplore_id"], axis = 1)
-    #tmp_class_table.replace(np.nan, 'Unknown')
-    #self.class_table = tmp_class_table
     self.class_table = class_table
     self.is_standard = is_standard
     self.spectra = spectra
