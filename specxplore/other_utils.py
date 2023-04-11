e213769489d20c82da4cca5bd4f23b0ad2e6baa3
Kevin Mildau <kevin.mildau@gmail.com>
2023-04-11 11:00:25 +0200

    migrated function temporarily

--- specxplore/other_utils.py
+++ specxplore/other_utils.py
@@ -10,3 +10,18 @@ def scale_array_to_minus1_plus1(array : np.ndarray) -> np.ndarray:
     # Normalised [-1,1]
     out = 2.*(array - np.min(array))/np.ptp(array)-1
     return out
+
+def initialize_cytoscape_graph_elements(tsne_df, selected_class_data, is_standard):
+    n_nodes = tsne_df.shape[0]
+    nodes = [{}] * n_nodes 
+    for i in range(0, n_nodes):
+        if is_standard[i] == True:
+            standard_entry = " is_standard"
+        else:
+            standard_entry = ""
+        nodes[i] = {
+            'data':{'id':str(i)},
+            'classes': str(selected_class_data[i]) + standard_entry, #.replace('_',''), #  color_class[i],
+            'position':{'x':tsne_df["x"].iloc[i], 'y':-tsne_df["y"].iloc[i]}        
+        }
+    return nodes
