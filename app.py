7f743f87d76851fb660372d3aabca8b1936c4596
Kevin Mildau <kevin.mildau@gmail.com>
2023-04-13 11:39:40 +0200

    updated mouseover hover info event

--- app.py
+++ app.py
@@ -407,23 +407,31 @@ def update_threshold_trigger_handler(n_submit, new_threshold):
 @app.callback(
     Output("hover-info-panel", 'children'),
     Input('cytoscape-tsne', 'mouseoverNodeData'),
+    State('select_class_level_dropdown', 'value'),
     prevent_initial_call=True
 )
-def displaymouseoverData(data):
+def displaymouseoverData(data, selected_class_level):
     """ Callback Function renders class table information and id for hovered over node in text panel."""
     if data:
         spec_id = data['id']
-        print(data)
         node_class_info = GLOBAL_DATA.class_table.iloc[int(spec_id)].to_dict()
+
+        selected_class_info = GLOBAL_DATA.class_table.iloc[int(spec_id)].to_dict()[selected_class_level]
+        del node_class_info[selected_class_level]
         class_string_list = []
         for key in node_class_info:
-            print(key)
-            print(node_class_info[key])
             class_string_list.append(f"{key} : {node_class_info[key]} ")
         class_string = ", ".join(class_string_list)
-        print(class_string_list)
-        information_string = (f"spectrum_id: {spec_id}, classification: {class_string}")
-        return information_string
+        information_string_list = [
+            html.B(f"spectrum_id: {spec_id},"), 
+            html.Br(),
+            html.B(f"Selected ontology = {selected_class_level}"),
+            html.Br(),
+            html.B(f"Predicted ontology value: {selected_class_info}"),
+            html.Br(),
+            f"Other classification information: {class_string}"
+            ]
+        return information_string_list
 
 ########################################################################################################################
 # expand level control setting
