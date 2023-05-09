56822cfae34293b5a154e66ba225fed44b91e5ba
Kevin Mildau <kevin.mildau@gmail.com>
2023-05-09 15:35:09 +0200

    extended dependencies in setup.py

--- setup.py
+++ setup.py
@@ -13,14 +13,25 @@ module_paths = [os.path.join(directory_name, name) for name in module_name_list]
 setup(
     name="specXplore setup",
     ext_modules=cythonize(module_paths),
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     install_requires=[
         "dash",
         "cython",
         "plotly",
         "dash_cytoscape",
         "numpy",
-        "pandas"
+        "pandas",
+        "matchms",
+        "ms2query",
+        "dash_daq",
+        "dash_cytoscape",
+        'scipy',
+        'networkx',
+        'sklearn',
+        'scipy',
+        'kmedoids',
+        'spec2vec',
+        'ms2deepscore'
     ]
 )
 # Cleaning out .cpp files that are not needed after .so object construction.
