8bdd17cf106645b4c199c4c9cef7408a763ee911
Kevin Mildau <kevin.mildau@gmail.com>
2023-05-16 09:43:37 +0200

    added compose to generic utils

--- specxplore/other_utils.py
+++ specxplore/other_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from functools import reduce
 
 def standardize_array(array : np.ndarray):
     # mean centering and deviation scaling
@@ -25,3 +26,21 @@ def initialize_cytoscape_graph_elements(tsne_df, selected_class_data, is_standar
             'position':{'x':tsne_df["x"].iloc[i], 'y':-tsne_df["y"].iloc[i]},   
         }
     return nodes
+
+def compose_function(*func): 
+    """ Generic function composer making use of functools reduce. 
+    
+    :param *func: Any number n of input functions to be composed.
+    :returns: A new function object.
+
+    Notes:
+    Works well in conjunction with functools:partial, where functions can be composed using functions with partially
+    filled arguments. This is especially useful for small processing pipelines that are locally defined. e.g:
+    threshold_filter_07 = partial(threshold_filter_array, threshold = 0.7)
+    extract_top_5 = partial(extract_top_from_array, top_number = 5)
+    pipeline = compose(threshold_filter_07, extract_top_5)
+    Where pipeline is now a function with one argument (array) based on partial functions with default arguments set.
+    """
+    def compose(f, g):
+        return lambda x : f(g(x))   
+    return reduce(compose, func, lambda x : x)
