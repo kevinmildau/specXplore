a1dabe975edb6b63c4a2894201082f2a207041ba
Kevin Mildau <kevin.mildau@gmail.com>
2023-07-25 16:57:04 +0200

    cleaned code

--- specxplore/constants.py
+++ specxplore/constants.py
@@ -1,6 +1,2 @@
-
-
 # Dark2 categorical color scale from color brewer; colorblind friendly
-#COLORS = ["#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666"]
-#COLORS = ['#FF7F0E', '#1F77B4', '#FFD700', '#2CA02C', '#D62728', '#9467BD', '#8C564B', '#E377C2']
 COLORS = ["#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666"] # DARK2 ggplot
