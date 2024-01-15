3f8d873b9900292b2f9b66154dddb6c895a29aeb
Kevin Mildau <kevin.mildau@gmail.com>
2024-01-15 11:18:59 +0100

    changed integer types to signed longlong and np.int64 for fixing windows conversion issues

--- specxplore/importing_cython.pyx
+++ specxplore/importing_cython.pyx
@@ -6,8 +6,8 @@ from cython cimport boundscheck, wraparound
 
 #@cython.boundscheck(False)
 #@cython.wraparound(False)
-def construct_unique_pairwise_indices_array(n_nodes):
-    cdef long long[:, ::1] index_array 
+def construct_unique_pairwise_indices_array(signed long long n_nodes):
+    cdef signed long long[:, ::1] index_array 
     index_array = np.ascontiguousarray(np.vstack(np.triu_indices(n_nodes, k=1)).T)
     return index_array
 
@@ -17,19 +17,19 @@ def construct_long_format_sim_arrays(
         double[:,::1] similarity_matrix
     ):
     """ 
-    Constructs unqique index pair and value arrays for pairwise similarity matrix.
+    Constructs unique index pair and value arrays for pairwise similarity matrix.
     
     Returns:
         np.array, np.array, np.array
     """
     assert similarity_matrix.shape[0] == similarity_matrix.shape[1], "Similarity Matrix provided must be square."
-    cdef int n_nodes = int(similarity_matrix.shape[0])
-    cdef long long[:, ::1] index_array = construct_unique_pairwise_indices_array(n_nodes)
-    cdef int n_edges = index_array.shape[0]
+    cdef signed long long n_nodes = int(similarity_matrix.shape[0])
+    cdef signed long long[:, ::1] index_array = construct_unique_pairwise_indices_array(n_nodes)
+    cdef signed long long n_edges = index_array.shape[0]
     cdef double[::1] value_array = np.zeros(n_edges, dtype=np.double)
-    cdef long long[::1] source_array = np.zeros(n_edges, dtype=np.int64)
-    cdef long long[::1] target_array = np.zeros(n_edges, dtype=np.int64)
-    cdef int index
+    cdef signed long long[::1] source_array = np.zeros(n_edges, dtype=np.int64)
+    cdef signed long long[::1] target_array = np.zeros(n_edges, dtype=np.int64)
+    cdef signed long long index
     for index in range(0, n_edges):
         value_array[index] = similarity_matrix[index_array[index][0]][index_array[index][1]]
         source_array[index] = index_array[index][0]
