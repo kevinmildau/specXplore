568acaa3844eeb37f70fa81a864472d20cbed338
Kevin Mildau <kevin.mildau@gmail.com>
2024-01-15 14:52:47 +0100

    updated netview functions to use signed long long

--- specxplore/netview_cython.pyx
+++ specxplore/netview_cython.pyx
@@ -2,24 +2,25 @@
 # cython: c_string_type=unicode, c_string_encoding=utf8
 import cython
 from cython cimport boundscheck, wraparound
-from libcpp.string cimport string
 
 #@cython.boundscheck(False)
 #@cython.wraparound(False)
 def create_cluster_edge_list(
-        long long[:] sources, 
-        long long[:] targets, 
-        long long[:] cluster_selection
-        ):
+    signed long long[:] sources, 
+    signed long long[:] targets, 
+    signed long long[:] selection
+    ):
+    """ 
+    Function extracts all sources and targets for specific selection of ids. Threshold independent.
+    """
     assert sources.size == targets.size
     edges = [{}] * sources.size
-    cdef set cluster_set = set(cluster_selection)
-    cdef string edge_class
-    cdef int index
+    cdef set selection_set = set(selection)
+    cdef signed long long index
     for index in range(0, sources.size):
         source = sources[index]
         target = targets[index]
-        if (source in cluster_set and target in cluster_set):
+        if (source in selection_set and target in selection_set):
             edge_class = str("edge_within_set")
         else:
             edge_class = str("edge_out_of_set")
@@ -27,5 +28,5 @@ def create_cluster_edge_list(
             'data':{'id':str(source) + "-" + str(target), 
             'source':str(source),
             'target':str(target)},
-            'classes':edge_class}
+            'classes':str(edge_class)}
     return(edges)
