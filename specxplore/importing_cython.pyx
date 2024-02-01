61c11f3b133b6b8ccac9b4ea0734a12a95ead8eb
Kevin Mildau <kevin.mildau@gmail.com>
2024-02-01 02:43:46 -0800

    added int64 conversion steps

--- specxplore/importing_cython.pyx
+++ specxplore/importing_cython.pyx
@@ -18,7 +18,7 @@ def construct_unique_pairwise_indices_array(signed long long n_nodes):
     such that all unique pairs of indices are made (order independent, no ordered duplications of pairs).
     """
     cdef signed long long[:, ::1] index_array 
-    index_array = np.ascontiguousarray(np.vstack(np.triu_indices(n_nodes, k=1)).T)
+    index_array = np.ascontiguousarray(np.vstack(np.triu_indices(n_nodes, k=1)).T, dtype=np.int64)
     return index_array
 
 #@cython.boundscheck(False)
@@ -33,14 +33,14 @@ def construct_long_format_sim_arrays(
         np.array (signed long long), np.array (signed long long), np.array (double)
     """
     assert similarity_matrix.shape[0] == similarity_matrix.shape[1], "Similarity Matrix provided must be square."
-    cdef signed long long n_nodes = int(similarity_matrix.shape[0])
+    cdef signed long long n_nodes = np.int64(similarity_matrix.shape[0])
     cdef signed long long[:, ::1] index_array = construct_unique_pairwise_indices_array(n_nodes)
     cdef signed long long n_edges = index_array.shape[0]
     cdef double[::1] value_array = np.zeros(n_edges, dtype=np.double)
     cdef signed long long[::1] source_array = np.zeros(n_edges, dtype=np.int64)
     cdef signed long long[::1] target_array = np.zeros(n_edges, dtype=np.int64)
     cdef signed long long index
-    for index in range(0, n_edges):
+    for index in np.arange(0, n_edges, 1, dtype=np.int64):
         value_array[index] = similarity_matrix[index_array[index][0]][index_array[index][1]]
         source_array[index] = index_array[index][0]
         target_array[index] = index_array[index][1]
